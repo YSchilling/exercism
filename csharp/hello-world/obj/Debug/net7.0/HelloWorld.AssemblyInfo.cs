92888411aa5507eac62987da5b1294bb91265536
YSchilling <78358953+YSchilling@users.noreply.github.com>
2024-10-21 22:48:18 +0200

    did some exercises

--- csharp/hello-world/obj/Debug/net7.0/HelloWorld.AssemblyInfo.cs
+++ csharp/hello-world/obj/Debug/net7.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b82da68331a0f577ac443fc5bfcf290d90a6e62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0b61c1e9515e71b60ec4ee5115d638c7d335cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
